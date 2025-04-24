09ff8959c829a3c5ed3d759649e512e87cc1777c
Call Me "K <pujayantkumar@gmail.com>
2025-04-24 18:29:32 +0200

    Update Insert Interval.py

--- Day1/Insert Interval.py
+++ Day1/Insert Interval.py
@@ -13,3 +13,29 @@ Explanation: The new interval [4,9] overlaps with [3,5],[6,7],[8,10].
 Constraints:
 1 ≤ intervals.size() ≤  105
 0 ≤ start[i], end[i] ≤ 109
+
+Solution Code:
+Code -
+#User function Template for python3
+
+class Solution:
+    def insertInterval(self, intervals, newInterval):
+        result =[]
+        i = 0
+        n = len(intervals)
+        
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+            
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        result.append(newInterval)
+        
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+            
+        return result
