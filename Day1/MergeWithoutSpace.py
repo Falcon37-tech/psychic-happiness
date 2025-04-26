fb64616ceb85ee8541b307c44c0fa358c0d3d3b8
Call Me "K <pujayantkumar@gmail.com>
2025-04-26 15:18:47 +0200

    Create MergeWithoutSpace.py

---
+++ Day1/MergeWithoutSpace.py
@@ -0,0 +1,50 @@
+##Day 3
+Today's problem is -
+
+Given two sorted arrays a[] and b[] of size n and m respectively, the task is to merge them in sorted order without using any extra space. Modify a[] so that it contains the first n elements and modify b[] so that it contains the last m elements.
+
+Examples:
+
+Input: a[] = [2, 4, 7, 10], b[] = [2, 3]
+Output:
+2 2 3 4
+7 10
+Explanation: After merging the two non-decreasing arrays, we get, 2 2 3 4 7 10
+Input: a[] = [1, 5, 9, 10, 15, 20], b[] = [2, 3, 8, 13]
+Output:
+1 2 3 5 8 9
+10 13 15 20
+Explanation: After merging two sorted arrays we get 1 2 3 5 8 9 10 13 15 20.
+Input: a[] = [0, 1], b[] = [2, 3]
+Output:
+0 1
+2 3
+Explanation: After merging two sorted arrays we get 0 1 2 3.
+Constraints:
+1 <= a.size(), b.size() <= 105
+0 <= a[i], b[i] <= 107
+
+##Solution
+**Code** -
+
+class Solution:
+    
+    def mergeArrays(self, a, b):
+        
+        n = len(a)
+        m = len(b)
+        
+        i = n - 1
+        j = 0
+        
+        while i >= 0 and j < m:
+            if a[i] > b[j]:
+                a[i], b[j] = b[j], a[i]
+                
+            i -= 1
+            j += 1
+            
+        a.sort()
+        b.sort()
+        
+        return a,b
