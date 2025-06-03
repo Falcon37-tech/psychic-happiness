7bec6ef65d8de68fe7bf4b2967b06ea105588728
Call Me "K <pujayantkumar@gmail.com>
2025-06-03 10:34:29 +0200

    Create ReversedALinkedList.py

---
+++ Next40Days/ReversedALinkedList.py
@@ -0,0 +1,50 @@
+Today's Problem is-
+
+Given the head of a linked list, the task is to reverse this list and return the reversed head.
+
+Examples:
+
+Input: head: 1 -> 2 -> 3 -> 4 -> NULL
+Output: head: 4 -> 3 -> 2 -> 1 -> NULL
+Explanation:
+
+Input: head: 2 -> 7 -> 10 -> 9 -> 8 -> NULL
+Output: head: 8 -> 9 -> 10 -> 7 -> 2 -> NULL
+Explanation:
+
+Input: head: 2 -> NULL
+Output: 2 -> NULL
+Explanation:
+
+Constraints:
+1 <= number of nodes, data of nodes <= 105
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
+
+Solution
+Code -
+
+
+"""
+# Node Class
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+"""
+
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        current = head
+        
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        return prev
