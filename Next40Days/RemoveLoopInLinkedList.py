d1f7f28069eb18453bd59f56cc0019d2ca72b178
Call Me "K <pujayantkumar@gmail.com>
2025-06-11 11:20:38 +0200

    Create RemoveLoopInLinkedList.py

---
+++ Next40Days/RemoveLoopInLinkedList.py
@@ -0,0 +1,68 @@
+Today's Problem is -
+
+Given the head of a linked list that may contain a loop.  A loop means that the last node of the linked list is connected back to a node in the same list. The task is to remove the loop from the linked list (if it exists).
+
+Custom Input format:
+
+A head of a singly linked list and a pos (1-based index) which denotes the position of the node to which the last node points to. If pos = 0, it means the last node points to null, indicating there is no loop.
+
+The generated output will be true if there is no loop in list and other nodes in the list remain unchanged, otherwise, false.
+
+Examples:
+
+Input: head = 1 -> 3 -> 4, pos = 2
+Output: true
+Explanation: The linked list looks like
+
+A loop is present in the list, and it is removed.
+Input: head = 1 -> 8 -> 3 -> 4, pos = 0
+Output: true
+Explanation: 
+
+The Linked list does not contains any loop. 
+Input: head = 1 -> 2 -> 3 -> 4, pos = 1
+Output: true
+Explanation: The linked list looks like 
+
+A loop is present in the list, and it is removed.
+Constraints:
+1 ≤ size of linked list ≤ 105
+
+Solution
+
+Code -
+
+'''
+# node class:
+
+class Node:
+    def __init__(self,val):
+        self.next=None
+        self.data=val
+
+'''
+
+class Solution:
+    #Function to remove a loop in the linked list.
+    def removeLoop(self, head):
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return
+        
+        slow = head
+        if slow == fast:
+            while fast.next != slow:
+                fast = fast.next
+        else:
+            while slow.next != fast.next:
+                slow = slow.next
+                fast = fast.next
+                    
+        fast.next = None
