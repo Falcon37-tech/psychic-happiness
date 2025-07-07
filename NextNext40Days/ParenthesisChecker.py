b4784f18e6d4e1126bebbaf22ef8b236acec085f
Call Me "K <pujayantkumar@gmail.com>
2025-07-07 13:37:06 +0200

    Create ParenthesisChecker.py

---
+++ NextNext40Days/ParenthesisChecker.py
@@ -0,0 +1,43 @@
+Today's Problem is -
+
+Given a string s, composed of different combinations of '(' , ')', '{', '}', '[', ']', verify the validity of the arrangement.
+An input string is valid if:
+
+         1. Open brackets must be closed by the same type of brackets.
+         2. Open brackets must be closed in the correct order.
+
+Examples :
+
+Input: s = "[{()}]"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "[()()]{}"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "([]"
+Output: False
+Explanation: The expression is not balanced as there is a missing ')' at the end.
+Input: s = "([{]})"
+Output: False
+Explanation: The expression is not balanced as there is a closing ']' before the closing '}'.
+Constraints:
+1 ≤ s.size() ≤ 10^6
+s[i] ∈ {'{', '}', '(', ')', '[', ']'}
+
+
+Solution
+
+Code-
+class Solution:
+    def isBalanced(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
