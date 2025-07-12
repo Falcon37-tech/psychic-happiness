4606425844a25520bb929d8b6389f8f7cdab830b
Call Me "K <pujayantkumar@gmail.com>
2025-07-12 19:46:15 +0200

    Create MaxofMinForEveryWindowSize.py

---
+++ NextNext40Days/MaxofMinForEveryWindowSize.py
@@ -0,0 +1,58 @@
+Today's Problem is -
+
+Given an array of integers arr[], the task is to find the maximum of the minimum values for every possible window size in the array, where the window size ranges from 1 to arr.size().
+
+More formally, for each window size k, determine the smallest element in all windows of size k, and then find the largest value among these minimums where 1<=k<=arr.size().
+
+Examples :
+
+Input: arr[] = [10, 20, 30, 50, 10, 70, 30]
+Output: [70, 30, 20, 10, 10, 10, 10] 
+Explanation: 
+1. First element in output indicates maximum of minimums of all windows of size 1. Minimums of windows of size 1 are [10], [20], [30], [50], [10], [70] and [30]. Maximum of these minimums is 70. 
+2. Second element in output indicates maximum of minimums of all windows of size 2. Minimums of windows of size 2 are [10], [20], [30], [10], [10], and [30]. Maximum of these minimums is 30. 
+3. Third element in output indicates maximum of minimums of all windows of size 3. Minimums of windows of size 3 are [10], [20], [10], [10] and [10]. Maximum of these minimums is 20. 
+Similarly other elements of output are computed.
+Input: arr[] = [10, 20, 30]
+Output: [30, 20, 10]
+Explanation: First element in output indicates maximum of minimums of all windows of size 1. Minimums of windows of size 1 are [10] , [20] , [30]. Maximum of these minimums are 30 and similarly other outputs can be computed
+Constraints:
+1 <= arr.size() <= 10^5
+1 <= arr[i] <= 10^6
+
+Solution
+Code-
+
+class Solution:
+    def maxOfMins(self, arr):
+        n = len(arr)
+        left = [-1]*n
+        right = [n]*n
+        stack = []
+       
+        for i in range(n):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            if stack:
+                left[i] = stack[-1]
+            stack.append(i)
+            
+        stack.clear()
+        
+        for i in range(n-1, -1, -1):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            if stack:
+                right[i] = stack[-1]
+            stack.append(i)
+            
+        ans = [0]*(n+1)
+        
+        for i in range(n):
+            length = right[i] - left[i] - 1
+            ans[length] = max(ans[length], arr[i])
+            
+        for i in range(n-1, 0, -1):
+            ans[i] = max(ans[i], ans[i+1])
+        return ans[1:]
+
