83ca32871cdcc6a824b33fc90c0d57f022c0ff0d
Call Me "K <pujayantkumar@gmail.com>
2025-07-21 10:09:25 +0200

    Create LongestPalindromicSubsequence.py

---
+++ NextNext40Days/LongestPalindromicSubsequence.py
@@ -0,0 +1,48 @@
+Today's Problem is-
+
+Given a string s, return the length of the longest palindromic subsequence.
+
+A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements without changing the order of the remaining elements.
+
+A palindromic sequence is a sequence that reads the same forward and backward.
+
+Examples:
+
+Input: s = "bbabcbcab"
+Output: 7
+Explanation: Subsequence "babcbab" is the longest subsequence which is also a palindrome.
+Input: s = "abcd"
+Output: 1
+Explanation: "a", "b", "c" and "d" are palindromic and all have a length 1.
+Input: s = "agbdba"
+Output: 5
+Explanation: The longest palindromic subsequence is "abdba", which has a length of 5. The characters in this subsequence are taken from the original string "agbdba", and they maintain the order of the string while forming a palindrome.
+Constraints:
+1 ≤ s.size() ≤ 1000
+The string contains only lowercase letters.
+
+Solution
+
+Code-
+#User function Template for python3
+
+class Solution:
+
+    def longestPalinSubseq(self, s):
+        n = len(s)
+        dp = [[0]*n for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][i] = 1
+            
+        for cl in range(2, n+1):
+            for i in range(n-cl+1):
+                j = i+cl-1
+                if s[i]==s[j]:
+                    if cl==2:
+                        dp[i][j] = 2
+                    else:
+                        dp[i][j] = dp[i+1][j-1]+2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][n-1]
