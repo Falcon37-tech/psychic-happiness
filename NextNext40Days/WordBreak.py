6c7f2409e49240077dcff051da68da9062f19fff
Call Me "K <pujayantkumar@gmail.com>
2025-08-09 20:19:58 +0200

    Create WordBreak.py

---
+++ NextNext40Days/WordBreak.py
@@ -0,0 +1,37 @@
+Today's Problem is -
+
+You are given a string s and a list dictionary[] of words. Your task is to determine whether the string s can be formed by concatenating one or more words from the dictionary[].
+
+Note: From dictionary[], any word can be taken any number of times and in any order.
+
+Examples :
+
+Input: s = "ilike", dictionary[] = ["i", "like", "gfg"]
+Output: true
+Explanation: s can be breakdown as "i like".
+Input: s = "ilikegfg", dictionary[] = ["i", "like", "man", "india", "gfg"]
+Output: true
+Explanation: s can be breakdown as "i like gfg".
+Input: s = "ilikemangoes", dictionary[] = ["i", "like", "man", "india", "gfg"]
+Output: false
+Explanation: s cannot be formed using dictionary[] words.
+Constraints:
+1 ≤ s.size() ≤ 3000
+1 ≤ dictionary.size() ≤ 1000
+1 ≤ dictionary[i].size() ≤ 100
+
+Solution
+Code-
+lass Solution:
+    def wordBreak(self, s, dictionary):
+        wordset = set(dictionary)
+        n = len(s)
+        dp = [False]*(n+1)
+        dp[0] = True
+        
+        for i in range(1, n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordset:
+                    dp[i] = True
+                    break
+        return dp[n]
